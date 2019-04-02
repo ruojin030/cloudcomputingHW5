89e027dc1a036605cd7f1929c323fc9bf1ab5efc
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-04-02 00:22:38 -0400

    QAQQQQQQ

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.get('/retrieve',jsonParser,function(req,res){
     client.execute(query,params,{prepare :true}, function (err, result) {
         if(err) return console.log(err);
         var contents = result.first();
-        console.log(contents)
+        var file = JSON.stringify(contents);
+        console.log(file)
 
         //The row is an Object with column names as property keys. 
         res.writeHead(200, {'Content-Type': 'image/...' })
