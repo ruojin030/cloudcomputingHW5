dc8e232a880e522576a7e19d9b2212e082718c92
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-04-02 00:06:07 -0400

    QAQQQQQQ

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/retrieve',jsonParser,function(req,res){
     var filename = req.body.filename
     console.log(filename)
     const query = "SELECT contents FROM imgs WHERE filename = ?"
-    client.execute(query,filename,{prepare:true}, function (err, result) {
+    client.execute(query,req.body.filename,{prepare:true}, function (err, result) {
         if(err) return console.log(err);
         var contents = result[0];
         var type = contents.mimetype;
