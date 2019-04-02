332c06c3c53ba772d0ea6204c89e20104b71b235
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-04-02 00:58:34 -0400

    QAQQQQQQ

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.post('/deposit',upload.single('contents'),function(req,res){
     
 })
 app.get('/retrieve',upload.none(),function(req,res){
-    var filename = req.body.filename
+    //var filename = req.body.filename
     console.log(req.body)
-    console.log(filename)
+    //console.log(filename)
     const query = "SELECT contents FROM imgs WHERE filename = ?"
     var params = [filename]
     client.execute(query,params,{prepare :true}, function (err, result) {
